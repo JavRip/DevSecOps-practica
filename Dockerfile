bbea6a8091fef60c1429e0949d2a404599684f87
JavRip <javierripgar@gmail.com>
2026-02-24 19:01:15 +0100

    Cambiada versión de python en Dockerfile por 3.12-slim para evitar vulnerabilidades

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11-slim
+FROM python:3.12-slim
 
 # 2. Instalar dependencias y crear usuario en una sola capa
 RUN apt-get update && \
--- config.py
+++
@@ -1,3 +0,0 @@
-# Configuración de prueba con un secreto falso
-API_KEY = "sk_test_1234567890abcdef1234567890abcdef"
-DATABASE_PASSWORD = "contraseña_123"
